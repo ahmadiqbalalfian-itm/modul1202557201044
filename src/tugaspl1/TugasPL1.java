0449c61dd9b933227762b4433694d791a3ead5c8
ahmadiqbalalfian-itm <ahmadiqbalalfian@gmail.com>
2026-04-17 06:42:14 +0700

    tambah scanner

--- src/tugaspl1/TugasPL1.java
+++ src/tugaspl1/TugasPL1.java
@@ -1,8 +1,9 @@
 package tugaspl1;
-
+//kita pakai Scanne dulu, nanti baru ganti JOptionPane yaww
+import java.util.Scanner;
 public class TugasPL1 {
-
     public static void main(String[] args) {
+        Scanner fira = new Scanner(System.in);
         int nilai[][] = {{4,6,4,2,8,4,2,10},{4,6,4,2,8,4,2,10}};
         //variabel untuk menyimpan total semua elemen dalam array nilai
         double total = 0; //kuncinya
@@ -23,13 +24,8 @@ public class TugasPL1 {
         }
         System.out.println("Jumlah Keseluruhan adalah\t: " + total + "\nRata-Ratanya adalah\t\t: " + total / putaran);
         
-        /*lanjut mencari posisi index dengan JOptionPane!
-        karena susah, jadi dilewatin dulu yaww
-        */
-        
-
         /*menjumlahkan semua eleemnt yang mempunyai indeks ganjil
-        apakah ngoding bareng bisa dilakukan realtime seperti canva, spreadsheet, doc dll?*/
+        Q apakah ngoding bareng bisa dilakukan realtime seperti canva, spreadsheet, doc dll?*/
         double totalg =0;
         for (int a = 0; a < nilai.length; a++) {
             for (int b = 0; b < nilai[a].length; b++) {
@@ -39,6 +35,16 @@ public class TugasPL1 {
                 }
             }
         }
-        System.out.println("Jumlah indeks ganjil adalah\t: "+totalg);   
+        System.out.println("Jumlah indeks ganjil adalah\t: "+totalg);  
+        
+        /*ini taruh bawah biar ngga error prosesnyaa
+        lanjut mencari posisi index dengan JOptionPane!
+        Kita masukkan pakai Scanner dulu
+        keknya sih logikanya pakai looping ganfda kayak buat print*/
+        System.out.println("Jika ada angka yang ingin diganti, masukkan satu angka\t:");
+        int ganti = firanext.Int;
+        
+        
+
     }
 }
