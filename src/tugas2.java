eafab9cd4b419eddf1df0be39d8244b98e62706d
ahmadiqbalalfian-itm <ahmadiqbalalfian@gmail.com>
2026-04-17 13:09:02 +0700

    lebih rapi

--- src/tugas2.java
+++ src/tugas2.java
@@ -1,6 +1,6 @@
 public class tugas2 {
     public static void main(String[] args) {
-        String data[][] = {{"NAMA", "ALAMAT", "\tTELEPON"}, {"ABDUL", "Kediri\t", "085646668991"}, {"KUSNO", "Trenggalek", "085646668992"},{"PONIRAN", "Bojonegoro", "085646668999"}};
+        String data[][] = {{"NAMA", "ALAMAT", "\tTELEPON"}, {"ABDUL", "Kediri", "\t085646668991"}, {"KUSNO", "Trenggalek", "085646668992"},{"PONIRAN", "Bojonegoro", "085646668999"}};
         for (int a = 0; a < data.length; a++) {
             for (int b = 0; b < data[a].length; b++) {
                 System.out.print(data[a][b] + "\t\t");
@@ -8,5 +8,4 @@ public class tugas2 {
             System.out.print("\n");
         }
     }
-
 }
